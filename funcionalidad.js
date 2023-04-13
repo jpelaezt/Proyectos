96794c40ee39dedb21d7e185479017ee2b4133a5
unknown <jpelaezt@est.ups.edu.ec>
2023-04-13 10:59:05 -0500

    cambios de pagina inicial para pruebas JS
    clase 13-04-2023

--- funcionalidad.js
+++ funcionalidad.js
@@ -17,17 +17,54 @@ function mostrar_imagen3(id) {
 	img = document.getElementById(id);
 	img.innerHTML = '<img id="img4" src="article_vinton-cerf_1.jpg" />';
 }
-
-alert("Hello! I am an alert box!!");
-console.log("Hello world!");
-
-var elemento = document.getElementById("btnBio1")
-elemento.addEventListener("click",function (){
-	alert("realizo cambio");
-})
 var i =0;
-var elemento = document.getElementById("btnBio1")
-elemento.addEventListener("mouseover",function (){
-	i = i + 1
-	console.log(i);
+
+//alert("Hello! I am an alert box!!");
+//console.log("Hello world!");
+
+var elemento = document.getElementById("t1")
+elemento.addEventListener("click",function (){
+	//alert("realizo cambio");
+	elemento.innerHTML = i
 })
+
+var elemento = document.getElementById("t1")
+elemento.addEventListener("mouseover",function (){
+	//i = i + 1
+	//console.log(i)
+	elemento.classList.add("body1")
+
+})
+
+
+var list = document.querySelectorAll("tabla cabecera td")
+console.log(list)
+
+var list2 = document.querySelectorAll("tabla cabecera td:nth-child(even)")
+console.log(list2)
+
+var list3 = document.querySelectorAll("tabla cabecera td:nth-child(add)")
+console.log(list3)
+
+//agregar etiquetas o crear elemento
+
+var parrafo = document.createElement("p");
+var cont = document.createTextNode("agrregando")
+parrafo.appendChild(cont)
+
+function addlist(){
+console.log("prueba");
+var link = document.createElement("td")
+link.innerHTML = "prueba"
+var ul = document.querySelector("tabla cabecera td")
+ul.appendChild(link)
+}
+
+//eliminar una clase
+
+var elementof = document.getElementById("btnBio5")
+elemento.addEventListener("click",function (){
+	//alert("realizo cambio");
+	addlist()
+})
+//elemento.classList.remove("clase css")
