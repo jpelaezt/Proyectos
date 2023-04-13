0e1a3b983b6fcf4ef9bf9da6200266f6474de615
unknown <jpelaezt@est.ups.edu.ec>
2023-04-12 21:11:15 -0500

    intento de pagina web
    agregar pagina a github

---
+++ funcionalidad.js
@@ -0,0 +1,19 @@
+function miFuncion() {
+	alert("Activaste la función miFuncion()");
+}
+function mostrar_imagen(id) {
+	img = document.getElementById(id);
+	img.innerHTML = '<img id="img1" src="descarga.jpg" />';
+}
+function mostrar_imagen1(id) {
+	img = document.getElementById(id);
+	img.innerHTML = '<img id="img2" src="descarga2.jpg" />';
+}
+function mostrar_imagen2(id) {
+	img = document.getElementById(id);
+	img.innerHTML = '<img id="img3" src="Dr_Vint_Cerf_ForMemRS.jpg" />';
+}
+function mostrar_imagen3(id) {
+	img = document.getElementById(id);
+	img.innerHTML = '<img id="img4" src="article_vinton-cerf_1.jpg" />';
+}
